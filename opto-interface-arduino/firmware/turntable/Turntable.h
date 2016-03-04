120aebccb6561a583ac1000fc52526fb6c4078f8
Jeremy Reeve <jeremy.reeve81@gmail.com>
2016-03-04 16:58:52 +0000

    Add spice schematic for ADC front-end sim.

--- opto-interface-arduino/firmware/turntable/Turntable.h
+++ opto-interface-arduino/firmware/turntable/Turntable.h
@@ -1,7 +1,6 @@
-
 /*                                                                      
  *
- * @(#)turntable.ino
+ * @(#)Turntable.h
  *
  * Copyright (c) 2016 Argusat Limited
  * 10 Underwood Road,  Southampton.  UK
@@ -22,23 +21,21 @@
   This example code is in the public domain.
 */
 
-#include <Servo.h>
-#include "Turntable.h"
+class Turntable
+{
+
+  public:
+    Turntable();
+    ~Turntable();
 
 
+  private:
 
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+    bool _direction;   // TRUE clockwise, FALSE counterclockwise
+    int  _current_angle;
+    int  _target_angle;
+
+    int _output_drive;
+
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
-}
